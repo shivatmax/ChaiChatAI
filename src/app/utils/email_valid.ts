67435f3b3135adcdf644d120fddbf5fb1cae7d92
shivatmax <awasthishiv0987@google.com>
2024-11-03 22:50:43 +0530

    refactor: minor fix

--- src/app/utils/email_valid.ts
+++ src/app/utils/email_valid.ts
@@ -1,4 +1,5 @@
 import { VerifaliaRestClient } from 'verifalia';
+import { logger } from './logger';
 
 const verifalia = new VerifaliaRestClient({
   username: 'shiv@m0.ventures',
@@ -8,7 +9,7 @@ const verifalia = new VerifaliaRestClient({
 export async function validateEmail(email: string): Promise<boolean> {
   try {
     const job = await verifalia.emailValidations.submit(email);
-    logger.debug(job);
+    logger.debug(`Email validation job: ${job}`);
     return job?.entries[0].status === 'Success';
   } catch (error) {
     // eslint-disable-next-line no-undef
